75fa559cc96a5cfec5d241dba0fe9ea113bb58d6
oscarpinzon <oskarin31@gmail.com>
2023-05-18 12:10:29 -0400

    add about and skills

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
 import Navbar from '../components/Navbar'
 import Main from '../components/Main'
+import About from '../components/About'
+import Skills from '../components/Skills'
 
 export default function Home() {
   return (
     <div>
       <Navbar />
       <Main />
+      <About />
+      <Skills />
     </div>
   )
 }
